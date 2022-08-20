a603f0ecf3c323b42007a01cd69810ea7f81be62
Johan PONIN <johan.ponin.pro@gmail.com>
2022-08-20 15:03:18 +0200

    feat(conf): toml conf definition and loading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use serde::Deserialize;
+use std::fs;
+use std::io::Error;
+
+#[derive(Deserialize, Debug)]
+struct LocalePurgeConf {
+    base: BaseConf,
+}
+
+#[derive(Deserialize, Debug)]
+struct BaseConf {
+    dir: String,
+    locale: String,
+    verbose: bool,
+    version: i32,
+}
+
+fn load() -> Result<LocalePurgeConf, Error> {
+    let toml_str = fs::read_to_string("./.localepurge.toml")?;
+    let map: LocalePurgeConf = toml::from_str(&toml_str)?;
+    Ok(map)
+}
 fn main() {
     println!("\nlocalepurge-rs\n");
 }
