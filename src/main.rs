7978a8dad2a286e6c6f2eadd4120dd3c0590ae65
Johan PONIN <johan.ponin.pro@gmail.com>
2022-08-20 18:53:33 +0200

    feat(threading): scoped thread fail

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use regex::Regex;
+use std::thread;
 use walkdir::WalkDir;
 
 mod config;
@@ -18,27 +19,31 @@ fn main() {
                 map.base.version, map.base.verbose,
             );
 
-            let mut avoided = 0;
-            let mut matched = 0;
+            thread::scope(|scope| {
+                let avoid = compile_re(map.locales.locales);
+                let re = Regex::new(avoid.as_str()).unwrap();
+                println!("excluding {}\n", avoid);
 
-            let avoid = compile_re(map.locales.locales);
-            let re = Regex::new(avoid.as_str()).unwrap();
-            println!("excluding {}\n", avoid);
-
-            for dir in map.locales.dirs {
-                let walker = WalkDir::new(dir).into_iter();
-                for entry in walker.filter_map(|e| e.ok()) {
-                    let ep = entry.path().to_string_lossy();
-                    if re.is_match(&ep) {
-                        avoided += 1;
-                        println!(". {}", ep)
-                    } else {
-                        matched += 1;
-                        println!("! {}", ep)
-                    }
+                for dir in map.locales.dirs {
+                    scope.spawn(move || {
+                        let mut l_avoided = 0;
+                        let mut l_matched = 0;
+                        let walker = WalkDir::new(&dir).into_iter();
+                        for entry in walker.filter_map(|e| e.ok()) {
+                            let ep = entry.path().to_string_lossy();
+                            if re.is_match(&ep) {
+                                l_avoided += 1;
+                                println!(". {}", ep)
+                            } else {
+                                l_matched += 1;
+                                println!("! {}", ep)
+                            }
+                        }
+                        println!("\n[{}]\n", dir);
+                        println!("\navoided: {}\nmatched: {}\n", l_avoided, l_matched,);
+                    });
                 }
-            }
-            println!("\navoided: {}\nmatched: {}\n", avoided, matched,);
+            })
         }
     }
 }
