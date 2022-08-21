4038942bb9a28c7c78713598f9d4a6abec27a9b8
Johan PONIN <johan.ponin.pro@gmail.com>
2022-08-21 13:17:16 +0200

    feat(logging): updated code to use env_logger

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use log::{debug, error, info};
 use regex::Regex;
 use walkdir::WalkDir;
 
@@ -7,32 +8,48 @@ fn compile_re(_locales: Vec<String>) -> String {
     String::from(".*/(fr|fr_FR|en|uk|ja)/?.*")
 }
 
-fn scan(dir: String, r: &Regex) {
+fn scan(dir: String, r: &Regex) -> (i32, i32) {
+    let mut avoided = 0;
+    let mut matched = 0;
     let walker = WalkDir::new(dir).into_iter();
     for e in walker.filter_map(|e| e.ok()) {
         let p = e.path().to_string_lossy();
         if r.is_match(&p) {
-            println!(". {}", p)
+            avoided += 1;
+            info!(". {}", p)
         } else {
-            println!("! {}", p)
+            matched += 1;
+            info!("! {}", p)
         }
     }
+    return (avoided, matched);
 }
 
 fn main() {
+    env_logger::init();
+
     println!("\nlocalepurge-rs © jnpn - 2022..<present>\n");
+
     match config::load() {
-        Err(e) => panic!("{:?}", e),
+        Err(e) => {
+            error!("fail");
+            panic!("{:?}", e);
+        }
         Ok(c) => {
-            println!("{:?}", c);
+            debug!("{:?}", c);
 
             let avoid = compile_re(c.locales.locales);
-            println!("excluding {}\n", avoid);
+            info!("excluding {}\n", avoid);
 
             let r = Regex::new(avoid.as_str()).unwrap();
 
+            let mut results: Vec<(i32, i32)> = vec![];
             for dir in c.locales.dirs {
-                scan(dir, &r);
+                results.push(scan(dir, &r));
+            }
+
+            for (a, m) in results {
+                info!("?: {}, {}", a, m);
             }
         }
     }
